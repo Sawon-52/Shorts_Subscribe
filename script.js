29b9ccdaf15f104b18888ac4ee17b97f2db363c1
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-02-05 19:09:22 +0600

    like button is working

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ subscribe.addEventListener('click', function(){
     subscribe.style.backgroundColor = 'rgb(235, 211, 211)';
     subscribe.style.opacity = '0.5';
 })
+
+
+
+const like = document.querySelector('#like')
+like.addEventListener('click', function(){
+    const likeShow = document.querySelector('#like_Show');
+    let insideValue = parseInt(likeShow.textContent);
+    let incrementValue = insideValue + 1;
+    likeShow.textContent = incrementValue;
+})
+
